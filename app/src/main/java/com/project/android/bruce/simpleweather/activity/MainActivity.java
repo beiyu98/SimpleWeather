91e1082226d2561163eff94337cd068554649f36
zhangshuaizone <zhangshuaizone@163.com>
2016-02-26 02:07:13 +0800

    ActionBar  按钮无法添加

--- app/src/main/java/com/project/android/bruce/simpleweather/activity/MainActivity.java
+++ app/src/main/java/com/project/android/bruce/simpleweather/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.project.android.bruce.simpleweather.activity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.project.android.bruce.simpleweather.R;
 
@@ -14,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.share:
+                return true;
+//            case R.id.add_location:
+//                return true;
+//            case R.id.setting:
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

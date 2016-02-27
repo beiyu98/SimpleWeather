580bb9b0dd4a725b8639a6bbeb0be7f5afbeb72f
zhangshuaizone <zhangshuaizone@163.com>
2016-02-27 19:38:22 +0800

    完成数据库的创建及操作类的创建

--- app/src/main/java/com/project/android/bruce/simpleweather/activity/MainActivity.java
+++ app/src/main/java/com/project/android/bruce/simpleweather/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.project.android.bruce.simpleweather.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.android.bruce.simpleweather.R;
 
@@ -12,11 +16,42 @@ import com.project.android.bruce.simpleweather.R;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private TextView city,weather,temperature,refreshTime;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+    }
+
+    private void initViews() {
+        city = (TextView) findViewById(R.id.city_name);
+        weather = (TextView) findViewById(R.id.weather);
+        temperature = (TextView) findViewById(R.id.temperature);
+        refreshTime = (TextView) findViewById(R.id.refresh_time);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        swipeRefreshLayout.setColorSchemeColors(
+                android.R.color.holo_red_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_orange_light);//暂时还未有反应
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(true);
+                    }
+                }, 5000);
+                Toast.makeText(MainActivity.this,"ok~~~~~~",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
